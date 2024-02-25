d2d7397aa231f34898d403ff0fb08c05cf19616f
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-02-25 15:06:45 +0300

    add Variables

---
+++ calculation.noise/cpp/variables/Variables.cpp
@@ -0,0 +1,10 @@
+#include "map"
+#include "string"
+
+namespace calculation {
+    const double max_distance_to_count = 0;
+
+    const std::map<std::string, double> planes_power = {
+            
+    };
+}
