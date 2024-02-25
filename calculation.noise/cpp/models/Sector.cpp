bf63ce332d3c736f93c3da920c2264a90301c174
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-02-25 19:25:30 +0300

    Add Sector class

---
+++ calculation.noise/cpp/models/Sector.cpp
@@ -0,0 +1,5 @@
+#include "Sector.h"
+
+namespace calculation {
+
+}
---
+++ calculation.noise/cpp/models/Sector.h
@@ -0,0 +1,16 @@
+#ifndef CPP_SECTOR_H
+#define CPP_SECTOR_H
+
+#include "Point.h"
+
+namespace calculation {
+
+    class Sector {
+        Point center;
+        Point leftDown;
+        Point RightUp;
+    };
+
+}
+
+#endif
