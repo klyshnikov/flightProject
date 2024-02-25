95421366f826e4cde00770d8f32c5abef4ae00da
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-02-25 16:45:16 +0300

    add algorithm to count distance

--- calculation.noise/cpp/algorithms/Algorithms.cpp
+++ calculation.noise/cpp/algorithms/Algorithms.cpp
@@ -1,9 +1,19 @@
-
 #include "Algorithms.h"
+#include <cmath>
 
 namespace calculation {
 
     double Algorithms::countDistanceBetweenPointAndFlight(Point point, const FlightFrame& flightFrame) {
+        double flightPointLatRad = (flightFrame.point.latitude * M_PI)/180.0;
+        double flightPointLonRad = (flightFrame.point.longitude * M_PI)/180.0;
+        double pointLatRad = (point.latitude * M_PI)/180.0;
+        double pointLonRad = (point.longitude* M_PI)/180.0;
+        double longitudeDelta = flightPointLonRad - pointLonRad;
+        double intermedValue1 = std::sin((flightPointLatRad - pointLatRad)/2.0) * std::sin((flightPointLatRad - pointLatRad)/2.0);
+        double intermedValue2 = std::cos(flightPointLatRad) * std::cos(pointLatRad) * std::sin(longitudeDelta/2.0) * std::sin(longitudeDelta/2.0);
+        double delta = 2.0*std::asin(std::sqrt(intermedValue1+intermedValue2));
+        double distanceOnGround = delta * 6372795.0;
 
+        return std::sqrt(distanceOnGround*distanceOnGround + flightFrame.altitude*flightFrame.altitude);
     }
 }
--- calculation.noise/cpp/variables/Variables.cpp
+++ calculation.noise/cpp/variables/Variables.cpp
@@ -5,6 +5,5 @@ namespace calculation {
     const double max_distance_to_count = 0;
 
     const std::map<std::string, double> planes_power = {
-            
     };
 }
