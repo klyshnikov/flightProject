ad2adb325fdd7664fc85ed90d1b9022f3c4b1d6f
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-02-25 15:04:11 +0300

    add Point model

---
+++ calculation.noise/cpp/models/Point.cpp
@@ -0,0 +1,7 @@
+
+#include "Point.h"
+
+namespace calculation {
+
+    Point::Point(double latitude, double longitude) : latitude(latitude), longitude(longitude) {}
+}
---
+++ calculation.noise/cpp/models/Point.h
@@ -0,0 +1,17 @@
+#ifndef CPP_POINT_H
+#define CPP_POINT_H
+
+namespace calculation {
+
+    class Point {
+    public:
+        double latitude;
+        double longitude;
+
+        Point(double latitude, double longitude);
+
+    };
+
+}
+
+#endif
