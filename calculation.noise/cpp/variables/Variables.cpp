8c2e6477ee2772a03f608e300ca701bc41b623e8
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-02-28 01:03:10 +0300

    add Sheremetyago coords

--- calculation.noise/cpp/variables/Variables.cpp
+++ calculation.noise/cpp/variables/Variables.cpp
@@ -2,6 +2,8 @@
 #include "string"
 
 namespace calculation {
+    const std::pair<double, double> sheremetyegoCords = {55.973233, 37.409741};
+
     const double max_distance_to_count = 0;
 
     const int shape_of_nearest_sectors = 3;
