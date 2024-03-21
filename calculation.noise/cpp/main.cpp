3888835ff6bdf6e529e50f14df55434d4b48d742
Mikhail Pavlov <mikhail.pavlov@corp.mail.ru>
2024-03-21 11:20:53 +0300

    print help

--- calculation.noise/cpp/main.cpp
+++ calculation.noise/cpp/main.cpp
@@ -13,7 +13,11 @@ int main(int argc, char** argv) {
 
     ArgumentsParser argumentsParser;
     if (!argumentsParser.parseArguments(argc, argv)) {
-        std::cout << "Wrong arguments\n";
+        std::cout << "Wrong arguments\n"
+                  << argv[0] << " <path_to_sheremetievo_history_directory> table <hour>\n"
+                  << " or \n"
+                  << argv[0] << " <path_to_sheremetievo_history_directory> sector <x> <y>\n";
+
     }
 
     /*
