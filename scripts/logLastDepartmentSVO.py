693c59746a91a7841540c18ad744be6401f92668
Mikhail Klyshnikov <mishakurg@gmail.com>
2024-04-02 12:52:44 +0300

    delete old folders

--- scripts/logLastDepartmentSVO.py
+++
@@ -1,7 +0,0 @@
-from Parsing.FlightawareParcer import FlightawareParser
-
-flightawareParaser = FlightawareParser()
-lastDepartmentSVO = flightawareParaser.getLastDepartmentInAirport("SVO")
-with open("../log.txt", 'a+') as file:
-    file.write(lastDepartmentSVO[0] + " " + lastDepartmentSVO[1])
-
--- sqlite3_realisation/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "sqlite3_library/sqlite3.h"
-
-int main() {
-
-
-    return 0;
-}
